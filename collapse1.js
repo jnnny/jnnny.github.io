2b9c73005dee00b14182239a6bd5ce80243eea9b
jnnny <42560943+jnnny@users.noreply.github.com>
2020-04-18 15:48:57 -0400

    siempre
    siempre.io - update after a year, skipped updating for Atelier

--- collapse1.js
+++ collapse1.js
@@ -1,3 +1,22 @@
 function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
+  var test = document.getElementById("demo");
+  if (test) {
+    test.addEventListener("click", function() {
+      console.log(test);
+      test.innerHTML = "Paragraph changed.";
+    });
+  }
 }
+
+
+
+<p class="date-folder" data-deploy="2013">2013</p>
+<ul id="2013" style="display:none;">
+<li><a data-windowtype="1" href="/index/news/2013/lecture-by-jacques-herzog_140109.html" title="20 DEC 2013 - LECTURE BY JACQUES HERZOG 09 JAN">>&nbsp;20 DEC 2013 - LECTURE BY JACQUES HERZOG 09 JAN</a></li>
+<li><a data-windowtype="1" href="/index/news/2013/exhibition-m-plus-hong-kong_140109.html" title="20 DEC 2013 - EXHIBITION M+ HONG KONG 09 JAN">>&nbsp;20 DEC 2013 - EXHIBITION M+ HONG KONG 09 JAN</a></li>
+
+<li><a data-windowtype="1" href="/index/news/2018/language-restraint-for-prada.html" title="12 JAN 2018 - HERZOG & DE MEURON FOR PRADA">>&nbsp;12 JAN 2018 - HERZOG & DE MEURON FOR PRADA</a></li>
+</ul>
+<p class="date-folder open" data-deploy="2017">2017</p>
+<ul id="2017">
+<li><a data-windowtype="1" href="/index/news/2017/477-basel-nordspitze.html" title="12 DEC 2017 - BASEL NORDSPITZE">>&nbsp;12 DEC 2017 - BASEL NORDSPITZE</a></li>
